ab995a50d5acadfac8cd7e65efe2f00ec9e3b80e
met2001 <met4357@uncw.edu>
2024-09-27 15:00:00 -0400

    Updated Air class and Transportation class
    finished the getters and setters for transportation.

--- untitled/src/Air.java
+++ untitled/src/Air.java
@@ -1,5 +1,7 @@
+// Miles Tutt
 public class Air extends Transportation{
-    public Air(String vehicleType, int , int engines, double speed) {
-        super(vehicleType);
+
+    public Air(String vehicleType, Boolean rental, int speed, int numPassengers, double cost) {
+        super(vehicleType, rental, speed, numPassengers, cost);
     }
 }
--- untitled/src/Transportation.java
+++ untitled/src/Transportation.java
@@ -1,5 +1,5 @@
 // Jack Cole
-
+// Miles Tutt
 public class Transportation {
     String vehicleType;
     Boolean rental;
@@ -17,7 +17,31 @@ public class Transportation {
     public void setVehicleType(String vehicleType){
         this.vehicleType = vehicleType;
     }
+    public void setRental(Boolean rental){
+        this.rental = rental;
+    }
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
+    public void setCost(double cost){
+        this.cost = cost;
+    }
+    public void setNumPassengers(int numPassengers){
+        this.numPassengers = numPassengers;
+    }
     public String getVehicleType(){
         return this.vehicleType;
+    }
+    public Boolean getRental(){
+        return this.rental;
+    }
+    public int getSpeed(){
+        return this.speed;
+    }
+    public double getCost(){
+        return this.cost;
+    }
+    public int getNumPassengers(){
+        return this.numPassengers;
     }
 }
