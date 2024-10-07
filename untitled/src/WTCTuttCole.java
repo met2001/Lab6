e2d0c486f4dd4e946be252562386bfcab9fe9475
met2001 <met4357@uncw.edu>
2024-10-07 13:02:07 -0400

    Started the driver file

--- untitled/src/WTCTuttCole.java
+++ untitled/src/WTCTuttCole.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class WTCTuttCole {
     public static void main(String[] args){
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What transportation would you like to use? (Land, Air, Water): ");
+        String transportation = input.nextLine();
+        if(transportation.equals("Land")){
+            LandTransportation land = new LandTransportation(false,0,0,0);
+            System.out.println(land);
+
+        } else if (transportation.equals("Air")) {
+            AirTransportation air = new AirTransportation(false,0,0,0);
+            System.out.println(air);
+
+        } else if (transportation.equals("Water")) {
+            WaterTransportation water = new WaterTransportation(false,0,0,0);
+            System.out.println(water);
+        }
+        else {
+            System.out.println("Enter either (Land, Air, or Water)");
+        }
     }
 }
